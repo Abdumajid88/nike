9911baae053d093b18b9b1b76b6ad343c706be9f
jojojijo302 <MUSTANGUZB170@gmail.com>
2025-11-18 00:11:57 +0500

    qale

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import './App.css'
-import Card from './Card/Card'
+import "./App.css";
+import Card from "./Card/Card";
 
 function App() {
-
   return (
-  <>
-<div className="cards">
-    <Card  bgColor={"#CBCBCB"} btnColor={"#A5A5A5"}></Card>
-  <Card bgColor ={ '#5CA7FF'} btnColor={"#FFC700"}></Card>
-  <Card bgColor ={ '#D083FF'} btnColor={"##00FFB2"} ></Card>
-  <Card bgColor ={ '##FF99E2'} btnColor={"##FFF500"}></Card>
-  <Card bgColor ={ '##C5C1C7'} btnColor={"##00FFB2"}></Card>
-  <Card bgColor ={ '##5CFF62'} btnColor={"##FFC700"}></Card>
-</div>
-
-  </>  
-   )
+    <>
+      <div className="cards">
+        <Card bgColor={"#CBCBCB"} btnColor={"#A5A5A5"}></Card>
+        <Card bgColor={"#5CA7FF"} btnColor={"#FFC700"}></Card>
+        <Card bgColor={"#D083FF"} btnColor={"##00FFB2"}></Card>
+        <Card bgColor={"#FF99E2"} btnColor={"#A5A5A5"}></Card>
+        <Card bgColor={"#C5C1C7"} btnColor={"#FFC700"}></Card>
+        <Card bgColor={"#5CFF62"} btnColor={"##00FFB2"}></Card>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import "./Card.css"
-import img1 from "/public/Picture.png"
-function Card(bgColor,btnColor) {
+import "./Card.css";
+import img1 from "/public/Picture.png";
+function Card({ bgColor, btnColor }) {
   return (
-    <div style={{backgroundColor:bgColor}}>
-        <img src={img1} alt="" />
-        <h3>NIKE AIR FORCE 1 ‘07 (white/black)</h3>
-        <p>$89.99</p>
-        <p>I lost left shoe, don’t need this one anymore. Please buy it.</p>
-        <button style={{backgroundColor:btnColor}}>
-            <i></i>
-            Add to cart
-        </button>
+    <div style={{ backgroundColor: bgColor }}>
+      <img src={img1} alt="" />
+      <h3>NIKE AIR FORCE 1 ‘07 (white/black)</h3>
+      <p>$89.99</p>
+      <p>I lost left shoe, don’t need this one anymore. Please buy it.</p>
+      <button style={{ backgroundColor: btnColor }}>
+        <i></i>
+        Add to cart
+      </button>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
